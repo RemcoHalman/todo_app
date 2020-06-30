264662e272a053a356fad424dd85d1d3f68f989e
Remco Halman <remco@mbp-van-remco.home>
2020-06-30 20:22:14 +0200

    first full deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn TodoApp.wsgi --log-file -
+web: gunicorn todoApp.wsgi --log-file -
--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -1,7 +1,17 @@
-from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib import admin
+
+admin.autodiscover()
+
+# To add a new path, first import the app:
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('todo.urls'))
+    path("admin/", admin.site.urls),
+    path('todo/', include('todo.urls'))
 ]
